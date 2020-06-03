b50ed28736cc1b0a3e3e808d2020913cf6a1e9ef
Maxime Schneider <m_schneider@etu-webschoolfactory.fr>
2020-06-03 14:56:14 +0200

    added jwtoken to store and oauth redirect component

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -5,7 +5,9 @@
         <v-flex>
           <v-subheader class="justify-center">Welcome to Youtube SubSearch</v-subheader>
           <v-subheader class="justify-center">Developped with VueJS and Golang using OAuth2</v-subheader>
-          <v-subheader class="justify-center"></v-subheader>
+          <v-subheader class="justify-center">The application is not registred as official Google Application</v-subheader>
+          <v-subheader class="justify-center">The scope of the user's Oauth consent is only for reading Youtube account informations</v-subheader>
+          <v-subheader class="justify-center">Source code is available at https://github.com/maximeschneider1/YoutubeApp</v-subheader>
         </v-flex>
       </v-layout>
     </v-container>
